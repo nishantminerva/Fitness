2e124e6caa0374b4ab82916811d54cc81d558ac5
Nishant <nishantminerva@gmail.com>
2024-03-25 16:24:29 +0530

    Initial Commit

---
+++ Fiitness/FiitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiitnessApp.swift
+//  Fiitness
+//
+//  Created by Nishant Minerva on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct FiitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
